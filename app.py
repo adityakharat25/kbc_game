201b3b539932801c040fe8699366f296ec79a58c
Aditya Kharat <adityakharat25@gmail.com>
2025-04-04 12:21:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def create_app(test_config=None):
     # Default config
     app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', '127.0.0.1')
     app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
-    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'Aditya@9476')
+    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'root')
     app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'kbc_game')
     app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
