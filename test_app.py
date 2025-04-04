eb8f157281cbb34907674ec9c826aa762061cb54
Aditya Kharat <adityakharat25@gmail.com>
2025-04-04 12:39:34 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,19 +1,12 @@
 import os
 import pytest
-from kbc_game.app import app, accepted_uid
+from kbc_game.app import create_app, accepted_uid
 from flask_mysqldb import MySQL
 from flask import session
 import werkzeug
 werkzeug.version="2.3.3"
 
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', '127.0.0.1')  # Use 127.0.0.1 instead of localhost
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'user')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'root')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'kbc_game')
-
-app.config['MYSQL_CURSORCLASS'] = 'DictCursor'  # Use dictionary cursor for query results
-
-mysql = MySQL(app)
+app = create_app()
 
 @pytest.fixture
 def client():
